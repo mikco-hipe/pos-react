65c471722701f2ba31e094ab613a5fced0b8b70d
karl-jerico <jerico.jeks@gmail.com>
2024-01-04 12:01:06 +0800

    Fix spacing

--- src/utils/icons.jsx
+++ src/utils/icons.jsx
@@ -1,19 +1,19 @@
-import * as FaIcons from 'react-icons/fa';
-import * as IoIcons from 'react-icons/io';
-import * as FiIcons from 'react-icons/fi';
-import * as BiIcons from 'react-icons/bi';
-import * as RxIcons from 'react-icons/rx';
+import * as FaIcons from "react-icons/fa"
+import * as IoIcons from "react-icons/io"
+import * as FiIcons from "react-icons/fi"
+import * as BiIcons from "react-icons/bi"
+import * as RxIcons from "react-icons/rx"
 
-export const Minus = () => <FaIcons.FaMinus  />;
-export const Add = () => <FaIcons.FaPlus  />; 
-export const Check = () => <FaIcons.FaCheck   />; 
-export const CheckBox = () => <FaIcons.FaCheckSquare   />; 
+export const Minus = () => <FaIcons.FaMinus />
+export const Add = () => <FaIcons.FaPlus />
+export const Check = () => <FaIcons.FaCheck />
+export const CheckBox = () => <FaIcons.FaCheckSquare />
 
 export const SearchIcon = () => <IoIcons.IoIosSearch />
-export const ArrowDown = () => <IoIcons.IoIosArrowDown  />;
+export const ArrowDown = () => <IoIcons.IoIosArrowDown />
 
-export const Menu = () => <FiIcons.FiMenu />;
+export const Menu = () => <FiIcons.FiMenu />
 
-export const Barcode = () => <BiIcons.BiBarcodeReader />;
+export const Barcode = () => <BiIcons.BiBarcodeReader />
 
-export const Cross = () => <RxIcons.RxCross1  />;
+export const Cross = () => <RxIcons.RxCross1 />
