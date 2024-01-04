f40f553ce1ce1fd334fc265e847f7ae94c6ee5a6
shiara02 <shiara@bpoc.co.jp>
2024-01-04 11:34:53 +0800

    initial environment setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT)
+  },
+  deifne: {
+    'process.env.PORT': `${process.env.PORT}`
+  }
 })
