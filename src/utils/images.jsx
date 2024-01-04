66b2183ca54009135cd5f42cfd3f758e86698c46
mikco-hipe <mikco@bpoc.co.jp>
2024-01-04 14:13:21 +0800

    :hammer: organize image imports with a dedicated utils file

---
+++ src/utils/images.jsx
@@ -0,0 +1,77 @@
+import banner1 from '@/assets/images/banners/food-banner-1.jpg'
+import banner2 from '@/assets/images/banners/food-banner-2.jpg'
+import banner3 from '@/assets/images/banners/food-banner-3.jpg'
+import banner4 from '@/assets/images/banners/food-banner-4.jpg'
+import banner5 from '@/assets/images/banners/food-banner-5.jpg'
+
+import dessert1 from '@/assets/images/desserts/dessert-1.jpg'
+import dessert2 from '@/assets/images/desserts/dessert-2.jpg'
+import dessert3 from '@/assets/images/desserts/dessert-3.jpg'
+import dessert4 from '@/assets/images/desserts/dessert-4.jpg'
+import dessert5 from '@/assets/images/desserts/dessert-5.jpg'
+import dessert6 from '@/assets/images/desserts/dessert-6.jpg'
+import dessert7 from '@/assets/images/desserts/dessert-7.jpg'
+import dessert8 from '@/assets/images/desserts/dessert-8.jpg'
+import dessert9 from '@/assets/images/desserts/dessert-9.jpg'
+import dessert10 from '@/assets/images/desserts/dessert-10.jpg'
+import dessert11 from '@/assets/images/desserts/dessert-11.jpg'
+import dessert12 from '@/assets/images/desserts/dessert-12.jpg'
+import dessert13 from '@/assets/images/desserts/dessert-13.jpg'
+import dessert14 from '@/assets/images/desserts/dessert-14.jpg'
+import dessert15 from '@/assets/images/desserts/dessert-15.jpg'
+import dessert16 from '@/assets/images/desserts/dessert-16.jpg'
+import dessert17 from '@/assets/images/desserts/dessert-17.jpg'
+import dessert18 from '@/assets/images/desserts/dessert-18.jpg'
+
+import bread1 from '@/assets/images/bread/bread-1.jpg'
+
+import drink1 from '@/assets/images/drinks/drink-1.jpg'
+
+import pasta1 from '@/assets/images/pasta/pasta-1.jpg'
+
+import pizza1 from '@/assets/images/pizza/pizza-1.jpg'
+
+export const BANNER_IMAGES = {
+  banner1,
+  banner2,
+  banner3,
+  banner4,
+  banner5
+}
+
+export const DESSERT_IMAGES = {
+  dessert1,
+  dessert2,
+  dessert3,
+  dessert4,
+  dessert5,
+  dessert6,
+  dessert7,
+  dessert8,
+  dessert9,
+  dessert10,
+  dessert11,
+  dessert12,
+  dessert13,
+  dessert14,
+  dessert15,
+  dessert16,
+  dessert17,
+  dessert18
+}
+
+export const BREAD_IMAGES = {
+  bread1
+}
+
+export const DRINK_IMAGES = {
+  drink1
+}
+
+export const PASTA_IMAGES = {
+  pasta1
+}
+
+export const PIZZA_IMAGES = {
+  pizza1
+}
