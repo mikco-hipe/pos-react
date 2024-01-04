221ba64a6fb9a045135c94112a3131f7be95c19d
karl-jerico <jerico.jeks@gmail.com>
2024-01-04 17:46:54 +0800

    convert jsx to tsx

---
+++ src/utils/icons.tsx
@@ -0,0 +1,19 @@
+import * as FaIcons from "react-icons/fa";
+import * as IoIcons from "react-icons/io";
+import * as FiIcons from "react-icons/fi";
+import * as BiIcons from "react-icons/bi";
+import * as RxIcons from "react-icons/rx";
+
+export const Minus: React.FC = () => <FaIcons.FaMinus />;
+export const Add: React.FC = () => <FaIcons.FaPlus />;
+export const Check: React.FC = () => <FaIcons.FaCheck />;
+export const CheckBox: React.FC = () => <FaIcons.FaCheckSquare />;
+
+export const SearchIcon: React.FC = () => <IoIcons.IoIosSearch />;
+export const ArrowDown: React.FC = () => <IoIcons.IoIosArrowDown />;
+
+export const Menu: React.FC = () => <FiIcons.FiMenu />;
+
+export const Barcode: React.FC = () => <BiIcons.BiBarcodeReader />;
+
+export const Cross: React.FC = () => <RxIcons.RxCross1 />;
