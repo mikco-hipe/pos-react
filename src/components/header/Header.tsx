8136b7703b9573aee350008b74e11add3e5fdf5d
shiara02 <shiara@bpoc.co.jp>
2024-01-05 17:23:13 +0800

    pop sidebar added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Cart, Menu, SearchIcon } from '@/utils/icons'
 import './header.scss'
+import Sidebar from '../sidebar/Sidebar';
 
 const Header: React.FC = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
-        <header className='menu'>
-            <nav>
-                <ul>
-                    <li className='menu_icon'>
-                        <Menu/>
-                    </li>
-                    <li className='menu_title'>
-                        Dashboard
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <form className='form'>
-                            <button><SearchIcon /></button>
-                            <input className='input' type="text" placeholder="Search" />
-                        </form>
-                    </li>
-                    <li className='cart-icon'>
-                        <Cart />
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <div className="header-container">
+            <header className='menu'>
+                <nav>
+                    <ul>
+                        <li className='menu_icon' onClick={toggleSidebar}>
+                            <Menu />
+                        </li>
+                        <li className='menu_title'>
+                            Page Name
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <form className='form'>
+                                <button><SearchIcon /></button>
+                                <input className='input' type="text" placeholder="Search" />
+                            </form>
+                        </li>
+                        <li className='cart-icon'>
+                            <Cart />
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <div className={`sidebar-container ${isSidebarOpen ? 'open' : ''}`}>
+                <Sidebar onClose={toggleSidebar} />
+            </div>
+        </div>
     )
 }
 
