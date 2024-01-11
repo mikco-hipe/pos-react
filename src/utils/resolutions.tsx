a6f63a4f2a8c1e375b5a64f8a488a88bf377dbaf
shiara02 <shiara@bpoc.co.jp>
2024-01-11 14:28:52 +0800

    UI modifications

--- src/utils/resolutions.tsx
+++ src/utils/resolutions.tsx
@@ -1,6 +1,6 @@
 const resolutions = [
     { breakpoint: 1824, slidesToShow: 5, slidesToScroll: 5 },
-    { breakpoint: 1535, slidesToShow: 4, slidesToScroll: 4 },
+    { breakpoint: 1535, slidesToShow: 2.85, slidesToScroll: 2.85 },
     { breakpoint: 1165, slidesToShow: 3, slidesToScroll: 3 },
     { breakpoint: 1023, slidesToShow: 2.5, slidesToScroll: 2 },
     { breakpoint: 819, slidesToShow: 2, slidesToScroll: 2 },
