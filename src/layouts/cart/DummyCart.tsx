704d1c84006fe911f48210dea84632237d4257d9
shiara02 <shiara@bpoc.co.jp>
2024-01-12 13:52:13 +0800

    created dummy cart

---
+++ src/layouts/cart/DummyCart.tsx
@@ -0,0 +1,21 @@
+export const DUMMY_CART = [
+    {
+        image: 'https://iamhomesteader.com/wp-content/uploads/2023/05/asiago-chicken-4.jpg',
+        name: 'Chicken Pasta',
+        price: 380.75,
+        quantity: 3,
+    },
+
+    {
+        image: 'https://i0.wp.com/smittenkitchen.com/wp-content/uploads//2017/05/strawberry-graham-icebox-cake.jpg?fit=1200%2C800&ssl=1',
+        name: 'Graham Cake',
+        price: 720.40,
+        quantity: 2,
+    },
+    {
+        image: 'https://img.freepik.com/premium-photo/glass-cocktail-decorated-with-fruits-bar-counter-background_127657-7186.jpg?size=626&ext=jpg',
+        name: 'Four Seasons',
+        price: 210.00,
+        quantity: 4,
+    },
+]
